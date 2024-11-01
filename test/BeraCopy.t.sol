8fa72ec61e66c27d35a58fe006575b13e576f821
cuongtq1026 <cuongtq1026@gmail.com>
2024-11-01 14:38:50 +0700

    test: add owner test

---
+++ test/BeraCopy.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BeraCopy} from "../src/BeraCopy.sol";
+
+contract BeraCopyTest is Test {
+    BeraCopy public beraCopy;
+     address private MOCK_OWNER = address(0x123);
+
+    function setUp() public {
+        beraCopy = new BeraCopy(MOCK_OWNER);
+    }
+
+    function test_Owner() public {
+        assertEq(beraCopy.owner(), MOCK_OWNER);
+    }
+}
