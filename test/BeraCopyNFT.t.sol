2b109b7d1b86ecd334b92ec8598759a8272dca5d
cuongtq1026 <cuongtq1026@gmail.com>
2024-11-04 18:27:32 +0700

    test: create test for BeraCopyNFT

---
+++ test/BeraCopyNFT.t.sol
@@ -0,0 +1,22 @@
+import {BeraCopyNFT} from "../src/BeraCopyNFT.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract BeraCopyTest is Test, BeraCopyNFT {
+    address private MOCK_OWNER = address(0x123);
+    uint256 private constant tokenId = 1;
+
+    function setUp() public {
+    }
+
+    function test_mintNFT() public {
+        _mintNFT(MOCK_OWNER, tokenId);
+        
+        assertEq(ownerOf(tokenId), MOCK_OWNER);
+    }
+
+    function test_create() public {
+        uint256 newTokenId = create(MOCK_OWNER);
+        
+        assertEq(ownerOf(newTokenId), MOCK_OWNER);
+    }
+}
